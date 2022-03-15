ef562e45c91000686d7f72a9b1ae8705a4d5affc
Andrii Paslavsky <a.paslavsky@gmail.com>
2022-03-15 15:44:57 +0200

    Switching to the dependabot instead of com.github.ben-manes.versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ import org.gradle.api.tasks.bundling.Jar
 plugins {
     base
     kotlin("jvm") version "1.3.61" apply false
-    id("com.github.ben-manes.versions") version "0.27.0"
     id("co.uzzu.dotenv.gradle") version "2.0.0"
     `maven-publish`
 }
